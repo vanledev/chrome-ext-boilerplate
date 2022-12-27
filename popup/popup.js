dbbb6c3901fd3a8d8cf4deb34f218215f5366af7
tientnk <tientnk98@gmail.com>
2022-12-27 11:23:47 +0700

    Switch using storage to cookie

--- popup/popup.js
+++ popup/popup.js
@@ -1,23 +1,3 @@
-const mbApi = "MBApi";
-const saveMbApi = (apiKey) =>
-   new Promise((r) =>
-      chrome.storage.local.set({ [mbApi]: apiKey }).then(() => {
-         r(apiKey);
-      })
-   );
-const getMbApi = () =>
-   new Promise((r) =>
-      chrome.storage.local.get(mbApi).then((result) => {
-         r(result[mbApi]);
-      })
-   );
-const removeMbApi = () =>
-   new Promise((r) =>
-      chrome.storage.local.remove(mbApi).then((result) => {
-         r(result);
-      })
-   );
-
 $(document).on("click", "#save", async function () {
    const value = $("#api_key").val();
    if (!value.includes("etsyapi")) {
@@ -25,26 +5,26 @@ $(document).on("click", "#save", async function () {
       return;
    }
    $(this).addClass("loader");
-   await removeMbApi();
-   await saveMbApi(value);
    chrome.runtime.sendMessage({
-      message: "saveApiKey",
+      message: "popupSaveApiKey",
       data: value,
    });
 });
 
-async function checkApiKey() {
-   const key = await getMbApi();
-   if (key) $("#api_key").val(key);
-}
-
 $(document).ready(function () {
-   checkApiKey();
+   $("#save").addClass("loader");
+   chrome.runtime.sendMessage({
+      message: "popupGetApiKey",
+   });
 });
 
 chrome.runtime.onMessage.addListener(function (request, sender, sendResponse) {
+   console.log("popup listing: ", request);
    if (request.message === "listedSaveApiKey") {
-      res({ message: "received" });
       window.close();
+   }
+   if (request.message === "popupGetApiKeyValue") {
+      $("#api_key").val(request.data);
+      $("#save").removeClass("loader");
    }
 });
